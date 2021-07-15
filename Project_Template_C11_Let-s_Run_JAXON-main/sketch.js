ab1801298a009880f78b4faac94cea1c4d6ed7de
shivanipt <87143211+shivanipt@users.noreply.github.com>
2021-07-15 11:55:42 -0500

    Jaxon upload #2

--- Project_Template_C11_Let-s_Run_JAXON-main/sketch.js
+++ Project_Template_C11_Let-s_Run_JAXON-main/sketch.js
@@ -1,26 +1,27 @@
-var track, invRightEdge, invLeftEdge;
-var player;
+var trackImage, track, track2, invRightEdge, invLeftEdge;
+var playerImage, player;
 
 function preload(){
   //pre-load images
-  track = loadImage("path.png");
-  player = loadAnimation("Runner-1.png", "Runner-2.png");
+  trackImage = loadImage("path.png");
+  playerImage = loadAnimation("Runner-1.png", "Runner-2.png");
 }
 
 function setup(){
   createCanvas(400,400);
   //create sprites here
 
-  //player sprite
-  player = createSprite(300, 100);
-  player.addAnimation("Runner-1.png", "Runner-2.png");
-
   //track sprite
-  track = createSprite(200, 200);
-  track.addImage("path.png");
+  track = createSprite(200, 100, 400, 400);
+  track.addImage("path.png", trackImage);
   track.y = track.length/2;
   track.velocityY = 4;
-  track.scale = 1.2
+  track.scale = 1.2;
+
+  //player sprite
+  player = createSprite(200, 200);
+  player.addAnimation("running", playerImage);
+  player.scale = 0.1;
 
   //createing invisible right edge
   invRightEdge = createSprite(10, 10, 10, 400);
